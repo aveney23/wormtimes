a84b488bee4ebcbbf07dd1bedf54dc9514a493a5
Aidan Veney <aidanveney@gmail.com>
2019-10-24 13:35:27 -0400

    Added basic yee-haw functionality

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const fs = require('fs')
+const commando = require("discord.js-commando")
+const client = new commando.CommandoClient();
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  
+  client.on('message', msg => {
+    if (msg.content === 'yee') {   //original responses
+      msg.reply('haw');
+    }
+  });
+  
+  client.login('NjM2OTcxMDkyMzY3Mzc2NDE0.XbHXpg.Gv2kopVwBdTF9kNtyEvZ_m8wvKA');
