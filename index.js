c159d86f2011a2cd40764e528ce4ffda3a14b1b6
Aidan Veney <aidanveney@gmail.com>
2019-10-25 09:45:51 -0400

    Added command/group registration and the ready command

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const commando = require("discord.js-commando")
-const client = new commando.CommandoClient();
+const path = require('path');
+const client = new commando.CommandoClient({
+  commandPrefix: '!',
+  owner: '202098742013198336'
+});
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
-  
+  client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['fun', 'Fun Commands']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
   client.on('message', msg => {
     if (msg.content === 'yee') {   //original responses
       msg.reply('haw');
     }
   });
   
-  client.login('NjM2OTcxMDkyMzY3Mzc2NDE0.XbHXpg.Gv2kopVwBdTF9kNtyEvZ_m8wvKA');
+  client.login('NjM2OTcxMDkyMzY3Mzc2NDE0.XbL76g.KfVrWLJa22gPiIdG_bhzY-K9Bxs');
