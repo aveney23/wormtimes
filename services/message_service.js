21ec2ba388df1d45f913ec7be82aa40723582a84
Aidan Veney <aidanveney@gmail.com>
2019-11-01 13:06:00 -0400

    added mod features to message service

--- services/message_service.js
+++ services/message_service.js
@@ -1,3 +1,10 @@
 module.exports = {
-    
+    analMess: function(message) {
+        if (message.content.includes("im a mod")) {
+            if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+                message.reply("no your not")
+            }
+            
+        }
+    }
 }
