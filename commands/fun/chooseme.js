bc594fe39adc2046dfca5589e174ed290199824a
Nasarocket04 <56931574+Nasarocket04@users.noreply.github.com>
2019-11-01 13:02:11 -0400

    Added Responses to 8ball

--- commands/fun/chooseme.js
+++ commands/fun/chooseme.js
@@ -11,11 +11,15 @@ module.exports = class RatCommand extends commando.Command {
         });
     }
     run (msg) {
-        let images = []
-        let image = images[Math.round(Math.random() * images.length)]
+        let text = ["Absolutely","My Sources Say Yes","Signs Point to Yes","why are you asking me?"
+        ,"The voices say no", "cannot predict now", "Don't Count on it", "Yes, Definitely", "You may rely on it",
+        "Ask again later", "My reply is no"
+    ]
+        let text = text[Math.round(Math.random() * images.length)]
         var embed= new Discord.RichEmbed()
         .setColor(any)
-        .setImage(image);
+        .setTitle(text);
+        .setImage("https://www.vermontcountrystore.com/ccstore/v1/images/?source=/file/v436715391556442924/products/51327.main.png&height=500&width=500&quality=0.88");
         msg.channel.sendEmbed(embed);
         
     }} 
