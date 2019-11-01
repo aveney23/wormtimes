eff11b4ee99cb1521e4fce9873c7dd436240beb5
Aidan Veney <aidanveney@gmail.com>
2019-11-01 10:59:12 -0400

    comment out token dependancy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const commando = require("discord.js-commando")
 const client = new commando.CommandoClient();
 const path = require('path')
-const token = require('./token')
+// const token = require('./token')
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
