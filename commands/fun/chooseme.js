3ed8ad7c4e0bd521b84dcefe3f84a639a4619dca
Nasarocket04 <56931574+Nasarocket04@users.noreply.github.com>
2019-11-01 13:07:05 -0400

    Made 8 ball final feature

--- commands/fun/chooseme.js
+++ commands/fun/chooseme.js
@@ -3,22 +3,22 @@ const Discord = require('discord.js')
 module.exports = class RatCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'jazzrat',
+            name: '8ball',
             group: 'fun',
-            memberName: 'jazzrat',
-            description: 'When you Say jazzrat it will show a different piture of rats',
+            memberName: '8ball',
+            description: 'When you Say 8ball and ask a question it will generate a response or answer',
             examples: ['rat']
         });
     }
     run (msg) {
         let text = ["Absolutely","My Sources Say Yes","Signs Point to Yes","why are you asking me?"
         ,"The voices say no", "cannot predict now", "Don't Count on it", "Yes, Definitely", "You may rely on it",
-        "Ask again later", "My reply is no"
+        "Ask again later", "My reply is no", "I have no clue man", "I mean, it could happen?", "For Certain ;)", "Yes Sir", "No Sir", "I mean... Sure" 
     ]
-        let text = text[Math.round(Math.random() * images.length)]
+        let text = text[Math.round(Math.random() * text.length)]
         var embed= new Discord.RichEmbed()
         .setColor(any)
-        .setTitle(text);
+        .setTitle(text)
         .setImage("https://www.vermontcountrystore.com/ccstore/v1/images/?source=/file/v436715391556442924/products/51327.main.png&height=500&width=500&quality=0.88");
         msg.channel.sendEmbed(embed);
         
