6c457ed84fce6edb06a264e6b38e914fdeca2aeb
Aidan Veney <aidanveney@gmail.com>
2019-11-03 10:40:29 -0500

    fixed code quality issues

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const commando = require("discord.js-commando")
 const client = new commando.CommandoClient();
-const path = require('path')
-// const token = require('./token')
-client.on('ready', () => {
+const path = require("path");
+// const token = require("./token")
+client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
-client.on('message', (message) => {
+client.on("message", () => {
   
 });
   client.registry
     .registerDefaultTypes()
     .registerGroups([
-        ['fun', 'Fun'],
-        ['debug', 'Debug']
+        ["fun", "Fun"],
+        ["debug", "Debug"]
     ])
     .registerDefaultGroups()
     .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+    .registerCommandsIn(path.join(__dirname, "commands"));
 
     // If you are cloning this repository for yourself, replace token.token with your token.
 
