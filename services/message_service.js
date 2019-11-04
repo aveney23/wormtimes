b2636174c6b7f1318eac9d6aa8d1ae936b7ec5d1
Aidan Veney <aidanveney@gmail.com>
2019-11-04 10:51:12 -0500

    added '

--- services/message_service.js
+++ services/message_service.js
@@ -1,6 +1,6 @@
 module.exports = {
     analMess(message) {
-        if (message.content.includes("im a mod")) {
+        if (message.content.includes("im a mod") | message.content.includes("i'm a mod")) {
             if (!message.member.hasPermission("MANAGE_MESSAGES")) {
                 message.reply("no you're not");
             }
