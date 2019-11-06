10c3e85f39ea3c208ee3f5b92166b9f91c3dc6e7
Nasarocket04 <56931574+Nasarocket04@users.noreply.github.com>
2019-11-06 13:03:20 -0500

    added varibles to fortune.js

--- commands/fun/future.js
+++ commands/fun/future.js
@@ -6,28 +6,27 @@ module.exports = class RatCommand extends commando.Command {
             name: "fortunecookie",
             group: "fun",
             memberName: "fortunecookie",
-<<<<<<< HEAD
-            description: "When you type !fortunecookie it will give you a fortune",
-            examples: ["you will be missed"]
-=======
             description: "Provides a fortune for you. Different fortune every time!",
             examples: ["!fortunecookie"]
->>>>>>> 960c5c439cfa049b2598ac97b78867eadc6849fa
         });
     }
     run (msg) {
-        let fortune = [
-            
-        ];
+        let fortune = ["run.","behind you","please wake up. This is not just a cookie. We are very worried about you.","The fortune you seek is in another cookie.",
+        "A closed mouth gathers no feet.",
+        "A conclusion is simply the place where you got tired of thinking.",
+        "A cynic is only a frustrated optimist.",
+        "A foolish man listens to his heart. A wise man listens to cookies.",
+        "You will die alone and poorly dressed.",
+        "A fanatic is one who can't change his mind, and won't change the subject.",
+        "If you look back, you'll soon be going that way.",
+        "You will live long enough to open many fortune cookies.",
+        "An alien of some sort will be appearing to you shortly.",
+        "Do not mistake temptation for opportunity.",
+        "Flattery will go far tonight.",
+          
+        ]
         let image = fortune[Math.round(Math.random() * fortune.length)];
         var embed= new Discord.RichEmbed()
         .setTitle(image);
         msg.channel.sendEmbed(embed);
-        
-<<<<<<< HEAD
-    }}; 
-
-  
-=======
-    }}; 
->>>>>>> 960c5c439cfa049b2598ac97b78867eadc6849fa
+    }}
