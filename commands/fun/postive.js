15b7ea3d18d1b720763a468c3d902a4e8ccd6884
Nasarocket04 <56931574+Nasarocket04@users.noreply.github.com>
2019-11-11 12:56:32 -0500

    set let command

--- commands/fun/postive.js
+++ commands/fun/postive.js
@@ -21,10 +21,10 @@ module.exports = class RatCommand extends commando.Command {
             "http://www.catersnews.com/wp-content/uploads/2016/11/Screen-Shot-2016-11-02-at-12.09.24-PM-800x498.png", "https://www.suggestedpost.eu/wp-content/uploads/2016/06/smiling-animals-1.jpeg",
             "https://www.suggestedpost.eu/wp-content/uploads/2016/06/smiling-animals-2.jpg", "https://i.ytimg.com/vi/cYNlJYQI3Uw/maxresdefault.jpg",
         ];
-        let  = pos[Math.round(Math.random() * pos.length)];
+        let woo  = pos[Math.round(Math.random() * pos.length)];
         let wow = woah[Math.round(Math.random() * woah.length)];
         var embed= new Discord.RichEmbed()
-        .setTitle(pos);
+        .setTitle(woo);
         setImage(wow);
         msg.channel.sendEmbed(embed);
     }}
